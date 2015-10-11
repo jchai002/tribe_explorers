d3b42250d03ee086bcb1506be02e6ca294f8d57d
Albalenys <albalenysd@gmail.com>
2015-10-11 09:39:03 -0400

    add user authentication

--- tribe_explorers/app/controllers/application_controller.rb
+++ tribe_explorers/app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout proc { false if request.xhr? }
+
+  def authenticate_user
+    redirect_to root_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
