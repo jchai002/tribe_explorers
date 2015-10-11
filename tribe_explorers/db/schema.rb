d2ec4136d986fb8bfc318b093da177ca446397f5
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-11 02:23:32 -0400

    yolo migration

--- tribe_explorers/db/schema.rb
+++ tribe_explorers/db/schema.rb
@@ -14,20 +14,21 @@
 ActiveRecord::Schema.define(version: 20151011061406) do
 
   create_table "cities", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string   "name"
     t.integer  "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "countries", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",              null: false
+    t.string   "firstname",         null: false
+    t.string   "lastname",          null: false
     t.string   "picture"
     t.string   "country_of_birth"
     t.string   "country_of_origin"
